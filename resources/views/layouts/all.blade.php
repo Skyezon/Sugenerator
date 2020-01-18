cf9b05faad88e0be3281ec0c5fa2debd92da3579
Delbert <richarddelbert84@gmail.com>
2020-01-18 14:56:47 +0700

    added htaccess redirect public for deployment

--- resources/views/layouts/all.blade.php
+++ resources/views/layouts/all.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{csrf_token()}}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
 
     <title>@yield('title'){{$title ?? ''}}</title>
