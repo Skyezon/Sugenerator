867329e8efc203e31ba64da9a0ddf927a49d3f77
Delbert <richarddelbert84@gmail.com>
2020-01-20 05:39:52 +0700

    testing for heroku

--- resources/views/sucika.blade.php
+++ resources/views/sucika.blade.php
@@ -59,7 +59,7 @@
         <div class="sucika__container">
             <h1 class="sucika__header">Hello {{$Internal->name}},</h1>
             <p class="sucika__body">{{$Internal->motivation}}</p>
-            <span class="sucika__footer">dari Richard</span>s
+            <span class="sucika__footer">dari Richard</span>
         </div>
         </div>
     </div>
