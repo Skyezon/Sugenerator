9264f48edde8a5f02b3052f54c83f5592ba8fa07
Delbert <richarddelbert84@gmail.com>
2020-03-06 20:58:40 +0700

    returning back to normal service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,12 +13,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
         Schema::defaultStringLength(191);
-        if($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
+        
     }
 
     /**
@@ -29,8 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if($this->app->environment('production')) {
-            $this->app['request']->server->set('HTTPS', true);
-        }
+       
     }
 }
